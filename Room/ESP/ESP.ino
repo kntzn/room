6f013372455bad81175cc7a6ec45fe05a693e0a3
kntzn <kntznart@gmail.com>
2018-11-01 15:13:56 +0300

    [Add]: esp example code

--- Room/ESP/ESP.ino
+++ Room/ESP/ESP.ino
@@ -1,107 +1,148 @@
-/*
- Name:		ESP.ino
- Created:	10/31/2018 6:13:41 PM
- Author:	CODEBOOK
-*/
-/*
-*  This sketch demonstrates how to set up a simple HTTP-like server.
-*  The server will set a GPIO pin depending on the request
-*    http://server_ip/gpio/0 will set the GPIO2 low,
-*    http://server_ip/gpio/1 will set the GPIO2 high
-*  server_ip is the IP address of the ESP8266 Arduino module, will be
-*  printed to Serial when the module is connected.
-*/
-
+// Load Wi-Fi library
 #include <ESP8266WiFi.h>
 
-const char* ssid = "your-ssid";
-const char* password = "your-password";
+// Replace with your network credentials
+const char* ssid = "Communism";
+const char* password = "00001111";
 
-// Create an instance of the server
-// specify the port to listen on as an argument
+// Set web server port number to 80
 WiFiServer server (80);
+
+// Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String output5State = "off";
+String output4State = "off";
+
+// Assign output variables to GPIO pins
+const int output5 = 2;
+const int output4 = 4;
 
 void setup () {
     Serial.begin (115200);
-    delay (10);
+    // Initialize the output variables as outputs
+    pinMode (output5, OUTPUT);
+    pinMode (output4, OUTPUT);
+    // Set outputs to LOW
+    digitalWrite (output5, LOW);
+    digitalWrite (output4, LOW);
 
-    // prepare GPIO2
-    pinMode (2, OUTPUT);
-    digitalWrite (2, 0);
-
-    // Connect to WiFi network
-    Serial.println ();
-    Serial.println ();
+    // Connect to Wi-Fi network with SSID and password
     Serial.print ("Connecting to ");
     Serial.println (ssid);
-
     WiFi.begin (ssid, password);
-
-    while (WiFi.status () != WL_CONNECTED)
-        {
+    while (WiFi.status () != WL_CONNECTED) {
         delay (500);
         Serial.print (".");
         }
+    // Print local IP address and start web server
     Serial.println ("");
-    Serial.println ("WiFi connected");
-
-    // Start the server
-    server.begin ();
-    Serial.println ("Server started");
-
-    // Print the IP address
+    Serial.println ("WiFi connected.");
+    Serial.println ("IP address: ");
     Serial.println (WiFi.localIP ());
+    server.begin ();
     }
 
-void loop () 
-    {
-    // Check if a client has connected
-    WiFiClient client = server.available ();
-    if (!client) 
-        {
-        return;
-        }
+void loop () {
+    WiFiClient client = server.available ();   // Listen for incoming clients
 
-    // Wait until the client sends some data
-    Serial.println ("new client");
-    while (!client.available ())
-        {
-        delay (1);
-        }
+    if (client) {                             // If a new client connects,
+        Serial.println ("New Client.");          // print a message out in the serial port
+        String currentLine = "";                // make a String to hold incoming data from the client
+        while (client.connected ()) {            // loop while the client's connected
+            if (client.available ()) {             // if there's bytes to read from the client,
+                char c = client.read ();             // read a byte, then
+                Serial.write (c);                    // print it out the serial monitor
+                header += c;
+                if (c == '\n') {                    // if the byte is a newline character
+                                                    // if the current line is blank, you got two newline characters in a row.
+                                                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length () == 0) {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println ("HTTP/1.1 200 OK");
+                        client.println ("Content-type:text/html");
+                        client.println ("Connection: close");
+                        client.println ();
 
-    // Read the first line of the request
-    String req = client.readStringUntil ('\r');
-    Serial.println (req);
-    client.flush ();
+                        // turns the GPIOs on and off
+                        if (header.indexOf ("GET /5/on") >= 0) {
+                            Serial.println ("GPIO 5 on");
+                            output5State = "on";
+                            digitalWrite (output5, HIGH);
+                            }
+                        else if (header.indexOf ("GET /5/off") >= 0) {
+                            Serial.println ("GPIO 5 off");
+                            output5State = "off";
+                            digitalWrite (output5, LOW);
+                            }
+                        else if (header.indexOf ("GET /4/on") >= 0) {
+                            Serial.println ("GPIO 4 on");
+                            output4State = "on";
+                            digitalWrite (output4, HIGH);
+                            }
+                        else if (header.indexOf ("GET /4/off") >= 0) {
+                            Serial.println ("GPIO 4 off");
+                            output4State = "off";
+                            digitalWrite (output4, LOW);
+                            }
 
-    // Match the request
-    int val = 0;
-    if (req.indexOf ("/gpio/0") != -1)
-        val = 0;
-    else if (req.indexOf ("/gpio/1") != -1)
-        val = 1;
-    else 
-        {
-        Serial.println ("invalid request");
+                        // Display the HTML web page
+                        client.println ("<!DOCTYPE html><html>");
+                        client.println ("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                        client.println ("<link rel=\"icon\" href=\"data:,\">");
+                        // CSS to style the on/off buttons 
+                        // Feel free to change the background-color and font-size attributes to fit your preferences
+                        client.println ("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                        client.println (".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+                        client.println ("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                        client.println (".button2 {background-color: #77878A;}</style></head>");
+
+                        // Web Page Heading
+                        client.println ("<body><h1>ESP8266 Web Server</h1>");
+
+                        // Display current state, and ON/OFF buttons for GPIO 5  
+                        client.println ("<p>GPIO 5 - State " + output5State + "</p>");
+                        // If the output5State is off, it displays the ON button       
+                        if (output5State == "off") {
+                            client.println ("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+                            }
+                        else {
+                            client.println ("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+                            }
+
+                        // Display current state, and ON/OFF buttons for GPIO 4  
+                        client.println ("<p>GPIO 4 - State " + output4State + "</p>");
+                        // If the output4State is off, it displays the ON button       
+                        if (output4State == "off") {
+                            client.println ("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+                            }
+                        else {
+                            client.println ("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+                            }
+                        client.println ("</body></html>");
+
+                        // The HTTP response ends with another blank line
+                        client.println ();
+                        // Break out of the while loop
+                        break;
+                        }
+                    else { // if you got a newline, then clear currentLine
+                        currentLine = "";
+                        }
+                    }
+                else if (c != '\r') {  // if you got anything else but a carriage return character,
+                    currentLine += c;      // add it to the end of the currentLine
+                    }
+                }
+            }
+        // Clear the header variable
+        header = "";
+        // Close the connection
         client.stop ();
-        return;
+        Serial.println ("Client disconnected.");
+        Serial.println ("");
         }
-
-    // Set GPIO2 according to the request
-    digitalWrite (2, val);
-
-    client.flush ();
-
-    // Prepare the response
-    String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nGPIO is now ";
-    s += (val) ? "high" : "low";
-    s += "</html>\n";
-
-    // Send the response to the client
-    client.print (s);
-    delay (1);
-    Serial.println ("Client disonnected");
-
-    // The client will actually be disconnected 
-    // when the function returns and 'client' object is detroyed
     }
+    //String s = "<!DOCTYPE html>/n/r<html>/n/r<head>/n/r<style>/n/rul {list-style-type: none; margin: 0; padding: 0; overflow: hidden; background-color: #333333; }/n/rli  { float: left; }/n/rli a { display: block; color: white; text-align: center; padding: 16px; text-decoration: none; }/n/rli a:hover { background-color: #111111; }/n/r</style>/n/r</head>/n/r<body style ='background-color: #222222'>/n/r<ul>/n/r<li><a href='setRGB/0'>Set 0</a></li>/n/r<li><a href='setRGB/1'>Set 1</a></li>/n/r<li><a href='setRGB/2'>Set 2</a></li>/n/r<li><a href='setRGB/3'>Set 3</a></li>/n/r</ul>/n/r<p>";
