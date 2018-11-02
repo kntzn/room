c6af32722ab5136d1ea5222b35449accaedaf146
kntzn <kntznart@gmail.com>
2018-11-02 15:17:10 +0300

    [Add]: website folder [Add]: server example code

--- Room/ESP/ESP.ino
+++ Room/ESP/ESP.ino
@@ -1,299 +1,139 @@
-/*
-===========================================
-Copyright (c) 2018 Stefan Kremser
-github.com/spacehuhn
-===========================================
-*/
-
-// ===== Settings ===== //
-const uint8_t channels[] = { 1, 6, 11 }; // used Wi-Fi channels (available: 1-14)
-const bool wpa2 = false; // WPA2 networks
-const bool appendSpaces = true; // makes all SSIDs 32 characters long to improve performance
-
-                                /*
-                                SSIDs:
-                                - don't forget the \n at the end of each SSID!
-                                - max. 32 characters per SSID
-                                - don't add duplicates! You have to change one character at least
-                                */
-const char ssids[] PROGMEM = {
-    "Mom Use This One\n"
-    "Abraham Linksys\n"
-    "Benjamin FrankLAN\n"
-    "Martin Router King\n"
-    "John Wilkes Bluetooth\n"
-    "Pretty Fly for a Wi-Fi\n"
-    "Bill Wi the Science Fi\n"
-    "I Believe Wi Can Fi\n"
-    "Tell My Wi-Fi Love Her\n"
-    "No More Mister Wi-Fi\n"
-    "LAN Solo\n"
-    "The LAN Before Time\n"
-    "Silence of the LANs\n"
-    "House LANister\n"
-    "Winternet Is Coming\n"
-    "Ping’s Landing\n"
-    "The Ping in the North\n"
-    "This LAN Is My LAN\n"
-    "Get Off My LAN\n"
-    "The Promised LAN\n"
-    "The LAN Down Under\n"
-    "FBI Surveillance Van 4\n"
-    "Area 51 Test Site\n"
-    "Drive-By Wi-Fi\n"
-    "Planet Express\n"
-    "Wu Tang LAN\n"
-    "Darude LANstorm\n"
-    "Never Gonna Give You Up\n"
-    "Hide Yo Kids, Hide Yo Wi-Fi\n"
-    "Loading…\n"
-    "Searching…\n"
-    "VIRUS.EXE\n"
-    "Virus-Infected Wi-Fi\n"
-    "Starbucks Wi-Fi\n"
-    "Text ###-#### for Password\n"
-    "Yell ____ for Password\n"
-    "The Password Is 1234\n"
-    "Free Public Wi-Fi\n"
-    "No Free Wi-Fi Here\n"
-    "Get Your Own Damn Wi-Fi\n"
-    "It Hurts When IP\n"
-    "Dora the Internet Explorer\n"
-    "404 Wi-Fi Unavailable\n"
-    "Porque-Fi\n"
-    "Titanic Syncing\n"
-    "Test Wi-Fi Please Ignore\n"
-    "Drop It Like It’s Hotspot\n"
-    "Life in the Fast LAN\n"
-    "The Creep Next Door\n"
-    "Ye Olde Internet\n"
-    };
-// ==================== //
-
-// ===== Includes ===== //
+// Load Wi-Fi library
 #include <ESP8266WiFi.h>
 
-extern "C" {
-#include "user_interface.h"
-    typedef void (*freedom_outside_cb_t)(uint8 status);
-    int wifi_register_send_pkt_freedom_cb (freedom_outside_cb_t cb);
-    void wifi_unregister_send_pkt_freedom_cb (void);
-    int wifi_send_pkt_freedom (uint8 *buf, int len, bool sys_seq);
-    }
-// ==================== //
+// Replace with your network credentials
+const char* ssid = "Communism";
+const char* password = "00001111";
 
-// run-time variables
-char emptySSID [32];
-uint8_t channelIndex = 0;
-uint8_t macAddr [6];
-uint8_t wifi_channel = 1;
-uint32_t currentTime = 0;
-uint32_t packetSize = 0;
-uint32_t packetCounter = 0;
-uint32_t attackTime = 0;
-uint32_t packetRateTime = 0;
+// Set web server port number to 80
+WiFiServer server (80);
 
-// beacon frame definition
-uint8_t beaconPacket [109] = {
-    /*  0 - 3  */ 0x80, 0x00, 0x00, 0x00, // Type/Subtype: managment beacon frame
-    /*  4 - 9  */ 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, // Destination: broadcast
-    /* 10 - 15 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // Source
-    /* 16 - 21 */ 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, // Source
+// Variable to store the HTTP request
+String header;
 
-                                                      // Fixed parameters
-                                                      /* 22 - 23 */ 0x00, 0x00, // Fragment & sequence number (will be done by the SDK)
-                                                      /* 24 - 31 */ 0x83, 0x51, 0xf7, 0x8f, 0x0f, 0x00, 0x00, 0x00, // Timestamp
-                                                      /* 32 - 33 */ 0xe8, 0x03, // Interval: 0x64, 0x00 => every 100ms - 0xe8, 0x03 => every 1s
-                                                      /* 34 - 35 */ 0x31, 0x00, // capabilities Tnformation
+// Auxiliar variables to store the current output state
+String output5State = "off";
+String output4State = "off";
 
-                                                                                // Tagged parameters
-
-                                                                                // SSID parameters
-                                                                                /* 36 - 37 */ 0x00, 0x20, // Tag: Set SSID length, Tag length: 32
-                                                                                /* 38 - 69 */ 0x20, 0x20, 0x20, 0x20,
-                                                                                0x20, 0x20, 0x20, 0x20,
-                                                                                0x20, 0x20, 0x20, 0x20,
-                                                                                0x20, 0x20, 0x20, 0x20,
-                                                                                0x20, 0x20, 0x20, 0x20,
-                                                                                0x20, 0x20, 0x20, 0x20,
-                                                                                0x20, 0x20, 0x20, 0x20,
-                                                                                0x20, 0x20, 0x20, 0x20, // SSID
-
-                                                                                                        // Supported Rates
-                                                                                                        /* 70 - 71 */ 0x01, 0x08, // Tag: Supported Rates, Tag length: 8
-                                                                                                        /* 72 */ 0x82, // 1(B)
-                                                                                                        /* 73 */ 0x84, // 2(B)
-                                                                                                        /* 74 */ 0x8b, // 5.5(B)
-                                                                                                        /* 75 */ 0x96, // 11(B)
-                                                                                                        /* 76 */ 0x24, // 18
-                                                                                                        /* 77 */ 0x30, // 24
-                                                                                                        /* 78 */ 0x48, // 36
-                                                                                                        /* 79 */ 0x6c, // 54
-
-                                                                                                                       // Current Channel
-                                                                                                                       /* 80 - 81 */ 0x03, 0x01, // Channel set, length
-                                                                                                                       /* 82 */      0x01,       // Current Channel
-
-                                                                                                                                                 // RSN information
-                                                                                                                                                 /*  83 -  84 */ 0x30, 0x18,
-                                                                                                                                                 /*  85 -  86 */ 0x01, 0x00,
-                                                                                                                                                 /*  87 -  90 */ 0x00, 0x0f, 0xac, 0x02,
-                                                                                                                                                 /*  91 -  92 */ 0x02, 0x00,
-                                                                                                                                                 /*  93 - 100 */ 0x00, 0x0f, 0xac, 0x04, 0x00, 0x0f, 0xac, 0x04, /*Fix: changed 0x02(TKIP) to 0x04(CCMP) is default. WPA2 with TKIP not supported by many devices*/
-                                                                                                                                                 /* 101 - 102 */ 0x01, 0x00,
-                                                                                                                                                 /* 103 - 106 */ 0x00, 0x0f, 0xac, 0x02,
-                                                                                                                                                 /* 107 - 108 */ 0x00, 0x00
-    };
-
-// goes to next channel
-void nextChannel () {
-    if (sizeof (channels) > 1) {
-        uint8_t ch = channels [channelIndex];
-        channelIndex++;
-        if (channelIndex > sizeof (channels)) channelIndex = 0;
-
-        if (ch != wifi_channel && ch >= 1 && ch <= 14) {
-            wifi_channel = ch;
-            wifi_set_channel (wifi_channel);
-            }
-        }
-    }
-
-// generates random MAC
-void randomMac () {
-    for (int i = 0; i < 6; i++)
-        macAddr [i] = random (256);
-    }
+// Assign output variables to GPIO pins
+const int output5 = 5;
+const int output4 = 4;
 
 void setup () {
-    // create empty SSID
-    for (int i = 0; i < 32; i++)
-        emptySSID [i] = ' ';
-
-    // for random generator
-    randomSeed (os_random ());
-
-    // set packetSize
-    packetSize = sizeof (beaconPacket);
-    if (wpa2) {
-        beaconPacket [34] = 0x31;
-        }
-    else {
-        beaconPacket [34] = 0x21;
-        packetSize -= 26;
-        }
-
-    // generate random mac address
-    randomMac ();
-
-    // start serial
     Serial.begin (115200);
-    Serial.println ();
+    // Initialize the output variables as outputs
+    pinMode (output5, OUTPUT);
+    pinMode (output4, OUTPUT);
+    // Set outputs to LOW
+    digitalWrite (output5, LOW);
+    digitalWrite (output4, LOW);
 
-    // get time
-    currentTime = millis ();
-
-    // start WiFi
-    WiFi.mode (WIFI_OFF);
-    wifi_set_opmode (STATION_MODE);
-
-    // set channel
-    wifi_set_channel (channels [0]);
-
-    // print out saved SSIDs
-    Serial.println ("SSIDs:");
-    int i = 0;
-    int len = sizeof (ssids);
-    while (i < len) {
-        Serial.print ((char)pgm_read_byte (ssids + i));
-        i++;
+    // Connect to Wi-Fi network with SSID and password
+    Serial.print ("Connecting to ");
+    Serial.println (ssid);
+    WiFi.begin (ssid, password);
+    while (WiFi.status () != WL_CONNECTED) {
+        delay (500);
+        Serial.print (".");
         }
-
-    Serial.println ();
-    Serial.println ("Started \\o/");
-    Serial.println ();
+    // Print local IP address and start web server
+    Serial.println ("");
+    Serial.println ("WiFi connected.");
+    Serial.println ("IP address: ");
+    Serial.println (WiFi.localIP ());
+    server.begin ();
     }
 
-void loop () {
-    currentTime = millis ();
+void loop () 
+    {
+    // Listen for incoming clients
+    WiFiClient client = server.available (); 
 
-    // send out SSIDs
-    if (currentTime - attackTime > 100) {
-        attackTime = currentTime;
+    // If a new client connects
+    if (client) 
+        {
+        // print a message out in the serial port
+        Serial.println ("New Client.");
+        
+        // make a String to hold incoming data from the client
+        String currentLine = "";                
+        
+        // while the client's connected
+        while (client.connected ()) 
+            {            
+            // if there's bytes to read from the client,
+            if (client.available ()) 
+                {
+                // read a byte, then
+                char c = client.read ();
+                // print it out the serial monitor
+                Serial.write (c);   
 
-        // temp variables
-        int i = 0;
-        int j = 0;
-        int ssidNum = 1;
-        char tmp;
-        int ssidsLen = strlen_P (ssids);
-        bool sent = false;
+                header += c;
+                // if the byte is a newline character
+                if (c == '\n') 
+                    {
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length () == 0) 
+                        {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println ("HTTP/1.1 200 OK");
+                        client.println ("Content-type:text/html");
+                        client.println ("Connection: close");
+                        client.println ();
 
-        // go to next channel
-        nextChannel ();
+                        // turns the GPIOs on and off
+                        if (header.indexOf ("GET /5/on") >= 0) 
+                            {
+                            Serial.println ("GPIO 5 on");
+                            output5State = "on";
+                            digitalWrite (output5, HIGH);
+                            }
+                        else if (header.indexOf ("GET /5/off") >= 0) 
+                            {
+                            Serial.println ("GPIO 5 off");
+                            output5State = "off";
+                            digitalWrite (output5, LOW);
+                            }
+                        else if (header.indexOf ("GET /4/on") >= 0) 
+                            {
+                            Serial.println ("GPIO 4 on");
+                            output4State = "on";
+                            digitalWrite (output4, HIGH);
+                            }
+                        else if (header.indexOf ("GET /4/off") >= 0) 
+                            {
+                            Serial.println ("GPIO 4 off");
+                            output4State = "off";
+                            digitalWrite (output4, LOW);
+                            }
 
-        while (i < ssidsLen) {
-            // read out next SSID
-            j = 0;
-            do {
-                tmp = pgm_read_byte (ssids + i + j);
-                j++;
-                } while (tmp != '\n' && j <= 32 && i + j < ssidsLen);
+                        // Display the HTML web page
+                        client.println ("<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1'><style>html { font-family: Helvetica;display: inline-block;margin: 0px auto;text-align: center;background-color: #151515;color: white; } .sidenav { height: 100%; width: 0; position: fixed; z-index: 1; top: 0; left: 0;background-color: #111;overflow-x: hidden;padding-top: 60px;transition: 0.5s; }.sidenav a{padding: 8px 8px 8px 32px;text-decoration: none;font-size: 25px;color: #818181;display: block;transition: 0.3s;}.sidenav a:hover {color: #f1f1f1;}.sidenav .closebtn {position: absolute;top: 0;right: 25px;font-size: 36px;margin-left: 50px;}</style></head><body><div id ='sideNav' class='sidenav':><a href ='javascript:void(0)'class='closebtn' onClick ='closeNav ()'>&times</a><a href ='light.html'>Light</a><a href ='light.html'>Light</a><a href ='light.html'>Light</a><a href ='light.html'>Light</a></div><div id ='main'><h1>Room remote</h1><span style='font-size:30px; cursor:pointer' onclick='openNav()'>&#9776; Menu</span></div><script>function openNav() {document.getElementById('sideNav').style.width = '400px';}</script><script>function closeNav() {document.getElementById('sideNav').style.width = '0';}</script></body></html>");
 
-                uint8_t ssidLen = j - 1;
-
-                // set MAC address
-                macAddr [5] = ssidNum;
-                ssidNum++;
-
-                // write MAC address into beacon frame
-                memcpy (&beaconPacket [10], macAddr, 6);
-                memcpy (&beaconPacket [16], macAddr, 6);
-
-                // reset SSID
-                memcpy (&beaconPacket [38], emptySSID, 32);
-
-                // write new SSID into beacon frame
-                memcpy_P (&beaconPacket [38], &ssids [i], ssidLen);
-
-                // set channel for beacon frame
-                beaconPacket [82] = wifi_channel;
-
-                // send packet
-                if (appendSpaces) {
-                    for (int k = 0; k<3; k++) {
-                        packetCounter += wifi_send_pkt_freedom (beaconPacket, packetSize, 0) == 0;
-                        delay (1);
+                        // The HTTP response ends with another blank line
+                        client.println ();
+                        // Break out of the while loop
+                        break;
                         }
+                    // if you got a newline, then clear currentLine
+                    else 
+                        currentLine = "";   
                     }
+                // if you got anything else but a carriage return character,
+                // add it to the end of the currentLine
+                else if (c != '\r')
+                    currentLine += c;
 
-                // remove spaces
-                else {
-
-                    uint16_t tmpPacketSize = (packetSize - 32) + ssidLen; // calc size
-                    uint8_t* tmpPacket = new uint8_t [tmpPacketSize]; // create packet buffer
-                    memcpy (&tmpPacket [0], &beaconPacket [0], 38 + ssidLen); // copy first half of packet into buffer
-                    tmpPacket [37] = ssidLen; // update SSID length byte
-                    memcpy (&tmpPacket [38 + ssidLen], &beaconPacket [70], wpa2 ? 39 : 13); // copy second half of packet into buffer
-
-                                                                                            // send packet
-                    for (int k = 0; k<3; k++) {
-                        packetCounter += wifi_send_pkt_freedom (tmpPacket, tmpPacketSize, 0) == 0;
-                        delay (1);
-                        }
-
-                    delete tmpPacket; // free memory of allocated buffer
-                    }
-
-                i += j;
+                }
             }
-        }
-
-    // show packet-rate each second
-    if (currentTime - packetRateTime > 1000) {
-        packetRateTime = currentTime;
-        Serial.print ("Packets/s: ");
-        Serial.println (packetCounter);
-        packetCounter = 0;
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop ();
+        Serial.println ("Client disconnected.");
+        Serial.println ("");
         }
     }
